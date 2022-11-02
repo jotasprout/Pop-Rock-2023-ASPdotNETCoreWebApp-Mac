d09651f88262506acd629f8663e2c23c51f2f0e7
Jay Sprout <jotasprout@gmail.com>
2022-11-02 09:21:07 -0400

    created Data and Entities classes

---
+++ API/Entities/Album.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string AlbumName { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
